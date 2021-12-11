4b215c1ced3086ed7c6c4941eb83c70652f01ef5
Earle-Poole <eepoole4@gmail.com>
2021-12-11 16:42:29 -0600

    Added support for the text-shadow className in Tailwind.

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -1,13 +1,13 @@
-import Logo from "@/components/atoms/Logo/Logo";
-import Navigation from "@/components/molecule/Navigation/Navigation";
+import Logo from '@/components/atoms/Logo/Logo'
+import Navigation from '@/components/molecule/Navigation/Navigation'
 
 const Header = () => {
   return (
-    <div className="bg-opacity-50 bg-gray-800 flex ">
+    <div className='bg-opacity-50 bg-gray-800 flex text-shadow'>
       <Logo />
       <Navigation />
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
