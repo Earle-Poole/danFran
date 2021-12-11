b4d954d33a505c44a768eaa5bbcd926d06624054
Margarita Poole <margaritapoole311@gmail.com>
2021-12-11 16:23:34 -0600

    added NavItem styles

--- components/atoms/NavItem/NavItem.tsx
+++ components/atoms/NavItem/NavItem.tsx
@@ -2,7 +2,7 @@ type NavItemProps = {name: string}
 
 const NavItem = (props: NavItemProps) => {
 return (
-    <div className="text-2xl font-light p-3 self-center">
+    <div className="text-2xl font-light p-3 self-center hover:text-pink-700 transition duration-150 hover:underline">
         {props.name}
     </div>
 )
