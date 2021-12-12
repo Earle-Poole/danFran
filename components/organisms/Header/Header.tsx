8359b0c70332a944e46961fe56f2f2e0b2bb3663
Earle-Poole <eepoole4@gmail.com>
2021-12-11 20:03:45 -0600

    Changed the way the useMediaQuery hook behaves to be more SSR friendly.

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -1,12 +1,15 @@
 import Logo from '@/components/atoms/Logo/Logo'
 import Navigation from '@/components/molecule/Navigation/Navigation'
 import { breakpoints } from 'lib/constants'
-import useMediaQuery from 'lib/hooks'
+import { useMediaQuery } from 'lib/hooks'
+import { useEffect } from 'react'
 
 const Header = () => {
   const isMobile = useMediaQuery(breakpoints.sm)
 
-  console.log('isMobile: ', isMobile)
+  useEffect(() => {
+    console.log('isMobile: ', isMobile)
+  }, [isMobile])
   return (
     <div className='bg-opacity-50 bg-gray-800 flex text-shadow'>
       <Logo />
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,53 +1,33 @@
 import { useEffect, useState } from 'react'
 import { BreakPointValues } from './constants'
-import { isClient, isApiSupported } from './toolbox'
 
-const errorMessage =
-  'matchMedia is not supported, this could happen both because window.matchMedia is not supported by' +
-  " your current browser or you're using the useMediaQuery hook whilst server side rendering."
-
-/**
- * Accepts a media query string then uses the
- * [window.matchMedia](https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia) API to determine if it
- * matches with the current document.<br />
- * It also monitor the document changes to detect when it matches or stops matching the media query.<br />
- * Returns the validity state of the given media query.
- *
- */
-const useMediaQuery = (mediaQuery: BreakPointValues): boolean | null => {
-  const [isVerified, setIsVerified] = useState(
-    !!window.matchMedia(mediaQuery).matches
-  )
+// See
+// https://www.netlify.com/blog/2020/12/05/building-a-custom-react-media-query-hook-for-more-responsive-apps/
+export const useMediaQuery = (query: BreakPointValues) => {
+  const [isMatch, setIsMatch] = useState(false)
 
   useEffect(() => {
-    const mediaQueryList = window.matchMedia(mediaQuery)
-    const documentChangeHandler = () => setIsVerified(!!mediaQueryList.matches)
+    const media = window.matchMedia(query)
 
-    try {
-      mediaQueryList.addEventListener('change', documentChangeHandler)
-    } catch (e) {
-      // Safari isn't supporting mediaQueryList.addEventListener
-      mediaQueryList.addListener(documentChangeHandler)
+    if (media.matches !== isMatch) {
+      setIsMatch(media.matches)
     }
 
-    documentChangeHandler()
-    return () => {
-      try {
-        mediaQueryList.removeEventListener('change', documentChangeHandler)
-      } catch (e) {
-        // Safari isn't supporting mediaQueryList.removeEventListener
-        mediaQueryList.removeListener(documentChangeHandler)
-      }
+    const listener = () => {
+      setIsMatch(media.matches)
     }
-  }, [mediaQuery])
 
-  if (!isClient || !isApiSupported('matchMedia')) {
-    // eslint-disable-next-line no-console
-    console.warn(errorMessage)
-    return null
-  }
+    // TODO: older Safari versions don't support addEventListener
+    // https://stackoverflow.com/questions/35719526/safari-ignore-window-matchmedia
+    if (media.addEventListener) {
+      media.addEventListener('change', listener)
+    } else if (media.addListener) {
+      // @ts-ignore
+      media.addListener(listener)
+    }
 
-  return isVerified
+    return () => media.removeEventListener?.('change', listener)
+  }, [isMatch, query])
+
+  return isMatch
 }
-
-export default useMediaQuery
