8a5afc7642932d724aee761783472d4c83f98d4a
Margarita Poole <margaritapoole311@gmail.com>
2021-12-19 15:15:19 -0600

    Added the About Me Photo to the About Me Section

--- components/molecule/AboutDanny/AboutDanny.tsx
+++ components/molecule/AboutDanny/AboutDanny.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image';
+import photo from '@/assets/about_me_photo1.jpg';
 import SocialMediaLinks from '@/components/atoms/Icons/SocialMediaLinks';
 
 const AboutDanny = () => {
   return (
     <div className='flex flex-row justify-center w-fit mx-auto'>
       <div></div>
-      <div>image goes here</div>
+      <Image src={photo} alt='about_me_photo1' />
       <div>
         Daniel Franzese Bio
         <SocialMediaLinks />
