2e9ef210f24f8e591a28d1be35b7a9fbcdd4361d
Margarita Poole <margaritapoole311@gmail.com>
2021-12-20 09:10:29 -0600

    Added template literals for the About Me title

--- components/molecule/AboutDanny/AboutDanny.tsx
+++ components/molecule/AboutDanny/AboutDanny.tsx
@@ -11,10 +11,10 @@ const AboutDanny = () => {
         alt='about_me_photo1'
         className='flex-1'
       />
-      <div className='flex flex-col w-220 p-5'>
-        <p className='text-4xl'>Actor, Writer, Director, Comedian & Activist.</p>
-        <Image src={design} alt='zigzag' className='my-6'></Image>
-        <p className=''>
+      <div className='flex flex-col w-220 ml-9'>
+        <p className='text-4xl'>{`Actor, Writer, Director, Comedian & Activist.`}</p>
+        <Image src={design} alt='zigzag'></Image>
+        <p className='text-2xl my-3 leading-loose '>
           {`Best known for his roles in Larry Clark's "Bully", and as Damian in
           Tina Fey's "Mean Girls". Danny is the creator of several live comedy
           shows including rock opera "Jersey Shoresical: A Frickin' Rock
