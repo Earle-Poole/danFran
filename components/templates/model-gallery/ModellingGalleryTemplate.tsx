3758a81acaefac9f341ea48714522644e827c385
Margarita Poole <margaritapoole311@gmail.com>
2022-01-02 16:21:32 -0600

    Added main and thumbnail photos to the Assets file. Created the Model-Gallery and Picture Components.

--- components/templates/model-gallery/ModellingGalleryTemplate.tsx
+++ components/templates/model-gallery/ModellingGalleryTemplate.tsx
@@ -1,5 +1,17 @@
+import ModelGalleryComponent from '@/components/molecule/ModelGallery/ModelGallery'
+
 const ModellingGalleryTemplate = () => {
-  return <div>Modelling Gallery Template</div>
+  return (
+    <div className="w-full flex-col mt-36 ">
+      <p className="text-left text-5xl indent-14 pb-3">
+        Daniel Franzese - Modelling Gallery
+      </p>
+      <p className="text-left text-xl indent-14 italic">{`"You are you. Now, isn't that pleasant?" -Dr. Seuss`}</p>
+      <div className='border-4 border-red-700 mt-24'>
+      <ModelGalleryComponent />
+      </div>
+    </div>
+  )
 }
 
 export default ModellingGalleryTemplate
