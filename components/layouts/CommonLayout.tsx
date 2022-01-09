6d56d971b4e12dc9510e17db35d198cb98423f3c
Earle-Poole <eepoole4@gmail.com>
2022-01-08 22:49:26 -0600

    Fix header losing it's element for monitoring scroll position.

--- components/layouts/CommonLayout.tsx
+++ components/layouts/CommonLayout.tsx
@@ -4,8 +4,8 @@ import Header from '@/components/organisms/Header'
 
 const CommonLayout: FC = ({ children }) => {
   return (
-    <div className="flex h-screen w-screen bg-black text-white overflow-hidden">
-      <div className="flex mx-auto w-full relative justify-center overflow-hidden">
+    <div className="flex h-screen w-screen bg-black text-white">
+      <div className="flex mx-auto w-full relative justify-center">
         <Header />
         <div
           id="layout-body"
