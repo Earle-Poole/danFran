72fb90eb2fa363fca1147f5e2346b07c025d4acc
Margarita Poole <margaritapoole311@gmail.com>
2022-01-10 22:34:03 -0600

    Corrected typo in page quote, Styled section headers

--- components/atoms/VideoPlayer/VideoPlayer.tsx
+++ components/atoms/VideoPlayer/VideoPlayer.tsx
@@ -1,8 +1,10 @@
 const VideoPlayer = () => {
+  const h2Styles = 'text-left text-4xl indent-14 my-8'
+
   return (
     <div className="flex flex-col justify-center">
       <div className="mx-auto">
-          <h2>Comedy</h2>
+        <h2 className={h2Styles}>Comedy</h2>
         <iframe
           width="600"
           height="400"
@@ -13,12 +15,12 @@ const VideoPlayer = () => {
         ></iframe>
       </div>
       <div className="mx-auto">
-          <h2>Press</h2>
+        <h2 className={h2Styles}>Press</h2>
         <iframe
           width="600"
           height="400"
-          src="https://www.youtube.com/embed/kgqXMpDuAA4" 
-          title="YouTube video player" 
+          src="https://www.youtube.com/embed/kgqXMpDuAA4"
+          title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         ></iframe>
--- components/templates/video-gallery/VideoGalleryTemplate.tsx
+++ components/templates/video-gallery/VideoGalleryTemplate.tsx
@@ -1,4 +1,4 @@
-import VideoPlayer from '@/components/atoms/VideoPlayer/VideoPlayer'
+import VideoPlayer from "@/components/atoms/VideoPlayer/VideoPlayer"
 
 const VideoGalleryTemplate = () => {
   return (
@@ -6,7 +6,7 @@ const VideoGalleryTemplate = () => {
       <p className="text-left text-5xl indent-14 pb-3">
         Daniel Franzese - Video Gallery
       </p>
-      <p className="text-left text-xl italic flex-wrap w-2/5 ml-14">{`""Be the change that you wish to see in the world." -Mahatma Gandhi`}</p>
+      <p className="text-left text-xl italic flex-wrap w-2/5 ml-14">{`"Be the change that you wish to see in the world." -Mahatma Gandhi`}</p>
       <VideoPlayer />
     </div>
   )
