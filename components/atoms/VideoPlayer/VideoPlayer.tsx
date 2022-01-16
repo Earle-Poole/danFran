f37a811404874bfb1958320b6574be2c1fa46633
Margarita Poole <margaritapoole311@gmail.com>
2022-01-16 14:38:43 -0600

    Added styles to the video gallery

--- components/atoms/VideoPlayer/VideoPlayer.tsx
+++ components/atoms/VideoPlayer/VideoPlayer.tsx
@@ -1,160 +1,150 @@
-import styles from './VideoPlayer.module.css'
-import classNames from 'classnames'
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
-import { isClient } from 'lib/toolbox'
-
-
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 const VideoPlayer = () => {
   const comedyVideoList = [
     {
-      src: 'https://www.youtube.com/embed/eac91tZsZMw',
+      id: 'eac91tZsZMw',
       videoTitle: 'Shit Italian Moms Say #1',
     },
     {
-      src: 'https://www.youtube.com/embed/LCPV0sl4GdE',
+      id: 'LCPV0sl4GdE',
       videoTitle: 'Shit Italian Moms Say #2',
     },
     {
-      src: 'https://www.youtube.com/embed/tSF7heNcXSk',
+      id: 'tSF7heNcXSk',
       videoTitle: 'Shit Italian Moms Say #3',
     },
     {
-      src: 'https://www.youtube.com/embed/DX2o3U8045U',
+      id: 'DX2o3U8045U',
       videoTitle: 'Stand Up Comedy Act',
     },
     {
-      src: 'https://www.youtube.com/embed/_PCPwmuiYL0',
+      id: '_PCPwmuiYL0',
       videoTitle: 'Italian MOMster: Danny Scares Mom!',
     },
     {
-      src: 'https://www.youtube.com/embed/m7FnCsSJ5WY',
+      id: 'm7FnCsSJ5WY',
       videoTitle: 'Italian Mom: Embarrassing Family Stories',
     },
   ]
 
-  const [selectedMainVideo, setSelectedMainVideo] = useState(null)
+  const pressVideoList = [
+    {
+      id: 'kgqXMpDuAA4',
+      videoTitle: 'Daniel Franzese Hosts the Voice Awards',
+    },
+    {
+      id: 'Li3Yl6RAcrU',
+      videoTitle: 'Daniel Franzese Joins Bootleg Fashion Photo Review',
+    },
+    {
+      id: 'ZqtkSped3K4',
+      videoTitle: 'Daniel Franzese on Hey Queen',
+    },
+    {
+      id: 'Bv9mhZqhb8Q',
+      videoTitle: "15 Things You Didn't Know About Daniel Franzese",
+    },
+    {
+      id: 'RpZ_70zuzqg',
+      videoTitle: 'Daniel Franzese Interview on Pop Trigger',
+    },
+  ]
+
+  const [selectedMainVideo, setSelectedMainVideo] = useState<videoSetupProps>(
+    comedyVideoList[0]
+  )
 
   useEffect(() => {
     console.log('selectedMainVideo', selectedMainVideo)
     return () => {}
   })
 
-  type videoSetupProps = {src: string, videoTitle: string}
+  type videoSetupProps = { id: string; videoTitle: string }
+
+  const h2Styles = 'text-left text-3xl indent-14 mt-8'
 
   return (
-    <div>
-      {comedyVideoList.map((videoSetup:videoSetupProps, i) => {
-        return (<div key={videoSetup.src}>
+    <div data-id="video container" className="flex w-full my-10">
+      {/* main video */}
+      <div
+        data-id="main video"
+        className="flex rounded-md p-3 border-solid border-2 border-sky-500"
+      >
+        <div key={selectedMainVideo.id}>
           <iframe
-          width="200"
-          height="100"
-          src={videoSetup.src}
-          title={videoSetup.videoTitle}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-        {`0${i+1}. ${videoSetup.videoTitle}`}
-        </div>)
-      })}
+            width="600"
+            height="400"
+            src={`https://www.youtube.com/embed/${selectedMainVideo.id}`}
+            title={selectedMainVideo.videoTitle}
+            allow="autoplay"
+            className="flex rounded-md justify-center"
+          ></iframe>
+          <span className={h2Styles}>{`${selectedMainVideo.videoTitle}`}</span>
+        </div>
+      </div>
+
+      <div
+        data-id="video list"
+        className="rounded-md h-96 overflow-y-scroll scroll-smooth scroll-px-2 scroll-w-4"
+      >
+        {/* comedy video list */}
+        <span className="text-left text-3xl indent-14 my-10">
+          Comedy Videos
+        </span>
+        {comedyVideoList.map((videoSetup: videoSetupProps, i) => {
+          const onClickHandler = () => {
+            setSelectedMainVideo(videoSetup)
+          }
+          return (
+            <div
+              key={videoSetup.id}
+              onClick={onClickHandler}
+              className="flex-col align-center gap-4 rounded-md m-2 p-2 cursor-pointer hover:bg-cyan-400 transition duration-150"
+            >
+              <Image
+                width="150"
+                height="75"
+                src={`https://img.youtube.com/vi/${videoSetup.id}/0.jpg`}
+                title={videoSetup.videoTitle}
+                alt="videoThumbnail"
+                className="rounded-md hover:text-cyan-400 transition duration-150"
+              />
+              <span className="flex text-lg text-pink-500">{`0${i + 1}. ${
+                videoSetup.videoTitle
+              }`}</span>
+            </div>
+          )
+        })}
+        {/* press videos list */}
+        <span className="text-left text-3xl indent-14 my-10">Press Videos</span>
+        {pressVideoList.map((videoSetup: videoSetupProps, i) => {
+          const onClickHandler = () => {
+            setSelectedMainVideo(videoSetup)
+          }
+          return (
+            <div
+              key={videoSetup.id}
+              onClick={onClickHandler}
+              className="flex-col align-center gap-4 rounded-md m-2 p-2 cursor-pointer hover:bg-cyan-400 transition duration-150"
+            >
+              <Image
+                width="150"
+                height="75"
+                src={`https://img.youtube.com/vi/${videoSetup.id}/0.jpg`}
+                title={videoSetup.videoTitle}
+                alt="videoThumbnail"
+                className="rounded-md hover:text-cyan-400 transition duration-150"
+              />
+              <span className="flex text-lg text-pink-500">{`0${
+                i + 1
+              }. ${videoSetup.videoTitle}`}</span>
+            </div>
+          )
+        })}
+      </div>
     </div>
-
-    //  <div className={classNames(styles.container)}>
-    //     {/* main video section */}
-    //     <div className={classNames(styles.mainVideo)}>
-    //       <div className={classNames(styles.video)}>
-    //         <iframe
-    //           width="600"
-    //           height="400"
-    //           src="https://www.youtube.com/embed/eac91tZsZMw"
-    //           title="Shit Italian Moms Say #1"
-    //           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>
-    //           01. Shit Italian Moms Say #1{' '}
-    //         </h3>
-    //       </div>
-    //     </div>
-
-    //     {/* video list section */}
-    //     <div className={classNames(styles.videoList)}>
-    //       <div className={classNames(styles.vid, styles.active)}>
-    //         <iframe
-    //           width="200"
-    //           height="100"
-    //           src="https://www.youtube.com/embed/eac91tZsZMw"
-    //           title="Shit Italian Moms Say #1"
-    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>
-    //           01. Shit Italian Moms Say #1{' '}
-    //         </h3>
-    //       </div>
-
-    //       <div className={classNames(styles.vid)}>
-    //         <iframe
-    //           width="200"
-    //           height="100"
-    //           src="https://www.youtube.com/embed/LCPV0sl4GdE"
-    //           title="Shit Italian Moms Say #2"
-    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>
-    //           02. Shit Italian Moms Say #2{' '}
-    //         </h3>
-    //       </div>
-
-    //       <div className={classNames(styles.vid)}>
-    //         <iframe
-    //           width="200"
-    //           height="100"
-    //           src="https://www.youtube.com/embed/tSF7heNcXSk"
-    //           title="Shit Italian Moms Say #3"
-    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>
-    //           03. Shit Italian Moms Say #3{' '}
-    //         </h3>
-    //       </div>
-
-    //       <div className={classNames(styles.vid)}>
-    //         <iframe
-    //           width="200"
-    //           height="100"
-    //           src="https://www.youtube.com/embed/DX2o3U8045U"
-    //           title="Stand Up Comedy Act"
-    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>04. Stand Up Comedy Act</h3>
-    //       </div>
-
-    //       <div className={classNames(styles.vid)}>
-    //         <iframe
-    //           width="200"
-    //           height="100"
-    //           src="https://www.youtube.com/embed/_PCPwmuiYL0"
-    //           title="Italian MOMster: Danny Scares Mom!"
-    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>
-    //           05. Italian MOMster: Danny Scares Mom!{' '}
-    //         </h3>
-    //       </div>
-
-    //       <div className={classNames(styles.vid)}>
-    //         <iframe
-    //           width="200"
-    //           height="100"
-    //           src="https://www.youtube.com/embed/m7FnCsSJ5WY"
-    //           title="Italian Mom: Embarrassing Family Stories"
-    //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //         ></iframe>
-    //         <h3 className={classNames(styles.title)}>
-    //           06. Italian Mom: Embarrassing Family Stories{' '}
-    //         </h3>
-    //       </div>
-    //     </div>
-    //   </div>
   )
 }
-
 export default VideoPlayer
