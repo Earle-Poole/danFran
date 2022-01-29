09505226878d0d5123393f9af38f04f5989efee6
Margarita Poole <margaritapoole311@gmail.com>
2022-01-29 15:57:19 -0600

    Styled the page headers for responsive viewing

--- components/templates/model-gallery/ModellingGalleryTemplate.tsx
+++ components/templates/model-gallery/ModellingGalleryTemplate.tsx
@@ -2,11 +2,11 @@ import ModelGallery from '@/components/organisms/ModelGallery/ModelGallery'
 
 const ModellingGalleryTemplate = () => {
   return (
-    <div className="w-full flex-col mt-36 ">
-      <p className="text-left text-3xl indent-2 md:text-5xl md:indent-14 pb-5">
+    <div className="w-full flex-col mt-36">
+      <p className="text-left text-3xl md:text-5xl indent-10 pb-5">
         Daniel Franzese - Modelling Gallery
       </p>
-      <p className="text-left text-lg indent-2 md:text-xl md:indent-14 italic">{`"You are you. Now, isn't that pleasant?" -Dr. Seuss`}</p>
+      <p className="text-left text-xl indent-10 italic">{`"You are you. Now, isn't that pleasant?" -Dr. Seuss`}</p>
       <ModelGallery />
     </div>
   )
