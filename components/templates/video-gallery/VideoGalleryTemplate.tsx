42764a2ca4268bfbc7e8010b814e4c29d78aad86
Margarita Poole <margaritapoole311@gmail.com>
2022-01-31 22:48:54 -0600

    Refined header spacing for mobile

--- components/templates/video-gallery/VideoGalleryTemplate.tsx
+++ components/templates/video-gallery/VideoGalleryTemplate.tsx
@@ -5,7 +5,7 @@ import classnames from 'classnames'
 const VideoGalleryTemplate = () => {
   return (
     <div className={classnames('w-full flex-col mt-36', styles.pageHeight)}>
-      <p className="text-left text-3xl md:text-5xl indent-4 md:indent-10 pb-5">
+      <p className="text-left text-3xl md:text-5xl ml-4 md:ml-10 pb-5">
         Daniel Franzese - Video Gallery
       </p>
       <p className="text-left text-xl ml-4 md:ml-10 italic flex-wrap md:w-3/5">{`"Be the change that you wish to see in the world." -Mahatma Gandhi`}</p>
