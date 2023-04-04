5f66e4be0bf5dc236d1b8611d7419961060fcdc4
Earle Poole <eepoole4@gmail.com>
2023-04-03 21:28:55 -0500

    Fix positioning of Modelling and Video Gallery headers

--- components/templates/model-gallery/ModelingGalleryTemplate.tsx
+++ components/templates/model-gallery/ModelingGalleryTemplate.tsx
@@ -2,11 +2,13 @@ import ModelGallery from '@/components/organisms/ModelGallery/ModelGallery'
 
 const ModelingGalleryTemplate = () => {
   return (
-    <div className="w-full flex-col mt-36">
-      <p className="text-left text-3xl md:text-5xl ml-4 md:ml-10 pb-5">
-        Daniel Franzese - Modeling Gallery
-      </p>
-      <p className="text-left text-xl ml-4 md:ml-10 italic">{`"You are you. Now, isn't that pleasant?" -Dr. Seuss`}</p>
+    <div className="w-full flex-col flex items-center mt-36">
+      <div className="w-full max-w-7xl">
+        <p className="text-left text-3xl md:text-5xl ml-4 md:ml-10 pb-5">
+          Daniel Franzese - Modeling Gallery
+        </p>
+        <p className="text-left text-xl ml-4 md:ml-10 italic">{`"You are you. Now, isn't that pleasant?" -Dr. Seuss`}</p>
+      </div>
       <ModelGallery />
     </div>
   )
--- components/templates/video-gallery/VideoGalleryTemplate.tsx
+++ components/templates/video-gallery/VideoGalleryTemplate.tsx
@@ -4,11 +4,18 @@ import classnames from 'classnames'
 
 const VideoGalleryTemplate = () => {
   return (
-    <div className={classnames('w-full flex-col mt-36', styles.pageHeight)}>
-      <p className="text-left text-3xl md:text-5xl ml-4 md:ml-10 pb-5">
-        Daniel Franzese - Video Gallery
-      </p>
-      <p className="text-left text-xl ml-4 md:ml-10 italic flex-wrap md:w-3/5">{`"Be the change that you wish to see in the world." -Mahatma Gandhi`}</p>
+    <div
+      className={classnames(
+        'w-full flex-col flex items-center mt-36',
+        styles.pageHeight
+      )}
+    >
+      <div className="w-full max-w-7xl">
+        <p className="text-left text-3xl md:text-5xl ml-4 md:ml-10 pb-5">
+          Daniel Franzese - Video Gallery
+        </p>
+        <p className="text-left text-xl ml-4 md:ml-10 italic flex-wrap md:w-3/5">{`"Be the change that you wish to see in the world." -Mahatma Gandhi`}</p>
+      </div>
       <VideoPlayer />
     </div>
   )
